86997b27d34f0f567bca22ec9eb74d56210fdd4e
Jiří Rája <469429@mail.muni.cz>
2024-07-19 11:27:41 +0200

    Added first draft of HTTP proxy

--- src/firehole/main.py
+++ src/firehole/main.py
@@ -1,4 +1,24 @@
+import time
+from multiprocessing import Process
+
+from firehole.http_s.proxy import run_server, Handler
 
 
 if __name__ == '__main__':
-    pass
+    processes = []
+
+    p = Process(target=run_server, args=("localhost", 4433, Handler.PROXY))
+    processes.append(p)
+    p.start()
+
+    p = Process(target=run_server, args=("localhost", 4434, Handler.TEST))
+    processes.append(p)
+    p.start()
+
+    try:
+        while True:
+            time.sleep(999)
+    except KeyboardInterrupt:
+        for p in processes:
+            p.kill()
+            p.join()
